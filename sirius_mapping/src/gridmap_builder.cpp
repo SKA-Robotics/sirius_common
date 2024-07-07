03879e86748f6702284128f64fbc10217dbd714f
Tomek Żebrowski <zebromek@gmail.com>
2024-07-07 20:51:46 +0200

    hopefully a better map updating conditions

--- sirius_mapping/src/gridmap_builder.cpp
+++ sirius_mapping/src/gridmap_builder.cpp
@@ -6,6 +6,7 @@
 
 std::string input_topic = "input_map";
 std::string output_topic = "output_map";
+float map_size, map_resolution, no_overwrite_time, uncertainty_threshold;
 
 ros::Publisher publisher;
 ros::Subscriber subscriber;
@@ -16,14 +17,17 @@ grid_map::GridMap localMap;
 void InitializeGlobalMap();
 void GridMapCallback(const grid_map_msgs::GridMap& msg);
 void PublishGlobalMap();
-bool ShouldCopyNewValue(float original_uncertainty, float new_uncertainty, float original_timestamp, float new_timestamp);
-bool ShouldClearValue(float original_uncertainty, float new_uncertainty, float original_timestamp, float new_timestamp);
 void ProcessMapsAtPosition(grid_map::GridMap& original_map, grid_map::GridMap& new_map, grid_map::Position position, float new_timestamp);
+void UpdateCell(grid_map::GridMap& map, grid_map::Position position, float new_traversability, float new_uncertainty, float new_timestamp);
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "gridmap_builder");
   ros::NodeHandle node_handle("~");
+  node_handle.param<float>("map_size", map_size, 30.0);
+  node_handle.param<float>("map_resolution", map_resolution, 0.05);
+  node_handle.param<float>("no_overwrite_time", no_overwrite_time, 7.0);
+  node_handle.param<float>("uncertainty_threshold", uncertainty_threshold, 1.0);
   InitializeGlobalMap();
   subscriber = node_handle.subscribe(input_topic, 1, GridMapCallback);
   publisher = node_handle.advertise<grid_map_msgs::GridMap>(output_topic, 5);
@@ -35,7 +39,7 @@ void InitializeGlobalMap()
 {
   globalMap = grid_map::GridMap({ "traversability", "uncertainty_range", "time" });
   globalMap.setFrameId("map");
-  globalMap.setGeometry(grid_map::Length(30, 30), 0.05, grid_map::Position(0, 0));
+  globalMap.setGeometry(grid_map::Length(map_size, map_size), map_resolution, grid_map::Position(0, 0));
 }
 
 void GridMapCallback(const grid_map_msgs::GridMap& msg)
@@ -64,51 +68,26 @@ void PublishGlobalMap()
   publisher.publish(message);
 }
 
-bool ShouldCopyNewValue(float original_uncertainty, float new_uncertainty, float original_timestamp, float new_timestamp)
-{
-  if (isnan(new_uncertainty))
-  {
-    return false;
-  }
-  if (isnan(original_uncertainty) || isnan(original_timestamp))
-  {
-    return true;
-  }
-  float time_diff = new_timestamp - original_timestamp;
-  float uncertainty_diff = new_uncertainty - original_uncertainty;
-  float uncertainty_penalty = 1.0f; // todo: make it a parameter
-  float time_penalty = 1.0f;
-  float penalty = uncertainty_penalty * uncertainty_diff - time_penalty * time_diff;
-  if (penalty < 0.0f)
-  {
-    return true;
-  }
-  return false;
-}
-
-bool ShouldClearValue(float original_uncertainty, float new_uncertainty, float original_timestamp, float new_timestamp) {
-  float uncertainty_threshold = 0.3f;
-  if (original_uncertainty > uncertainty_threshold) {
-    return true;
-  }
-  return false;
-}
-
 void ProcessMapsAtPosition(grid_map::GridMap& original_map, grid_map::GridMap& new_map, grid_map::Position position, float new_timestamp) {
-    float& original_traversability = original_map.atPosition("traversability", position);
+    float original_traversability = original_map.atPosition("traversability", position);
     float new_traversability = new_map.atPosition("traversability", position);
-    float& original_uncertainty = original_map.atPosition("uncertainty_range", position);
+    float original_uncertainty = original_map.atPosition("uncertainty_range", position);
     float new_uncertainty = new_map.atPosition("uncertainty_range", position);
-    float& original_timestamp = original_map.atPosition("time", position);
-    if (ShouldCopyNewValue(original_uncertainty, new_uncertainty, original_timestamp, new_timestamp))
-    {
-      original_traversability = new_traversability;
-      original_uncertainty = new_uncertainty;
-      original_timestamp = new_timestamp;
+    float original_timestamp = original_map.atPosition("time", position);
+
+    if (isnan(original_uncertainty) ||
+        (new_timestamp > original_timestamp + no_overwrite_time) ||
+        (new_uncertainty < original_uncertainty)) {
+      if (new_uncertainty < uncertainty_threshold) {
+        UpdateCell(original_map, position, new_traversability, new_uncertainty, new_timestamp);
+      } else {
+        UpdateCell(original_map, position, std::numeric_limits<float>::quiet_NaN(), new_uncertainty, new_timestamp);
+      }
     }
-    if (ShouldClearValue(original_uncertainty, new_uncertainty, original_timestamp, new_timestamp)) {
-      original_traversability = std::numeric_limits<double>::quiet_NaN();
-      original_uncertainty = std::numeric_limits<double>::quiet_NaN();
-      original_timestamp = new_timestamp;
-    }
+}
+
+void UpdateCell(grid_map::GridMap& map, grid_map::Position position, float new_traversability, float new_uncertainty, float new_timestamp) {
+    map.atPosition("traversability", position) = new_traversability;
+    map.atPosition("uncertainty_range", position) = new_uncertainty;
+    map.atPosition("time", position) = new_timestamp;
 }
