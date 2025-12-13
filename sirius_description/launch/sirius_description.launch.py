561f1ef7bd44c382ee6b36c2c82d4ce8f0d8315c
smuchars <01186981@pw.edu.pl>
2025-12-13 22:06:00 +0100

    Added 6DOF manipulator xacro

--- sirius_description/launch/sirius_description.launch.py
+++ sirius_description/launch/sirius_description.launch.py
@@ -1,7 +1,8 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
@@ -13,8 +14,17 @@ def generate_launch_description():
     pkg_path = get_package_share_directory(package_name)
     xacro_path = os.path.join(pkg_path, 'robots', xacro_file_name)
 
+    manipulator_version_arg = DeclareLaunchArgument(
+        'manipulator',
+        default_value='none',
+        description='Manipulator verion',
+        choices=['none', '5DOF', '6DOF']
+    )
+
     robot_description = ParameterValue(
-        Command(['xacro ', xacro_path]),
+        Command(['xacro ', xacro_path,
+                 ' manipulator:=', LaunchConfiguration('manipulator')
+                 ]),
         value_type=str
     )
 
@@ -26,5 +36,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        manipulator_version_arg,
         robot_state_publisher_node
     ])
